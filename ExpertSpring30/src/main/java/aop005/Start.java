eafb4a7585f42859d0f0cde9b3b66d196dada936
SH <scardzem@naver.com>
2023-05-12 18:33:11 +0900

    pc-nb test

--- ExpertSpring30/src/main/java/aop005/Start.java
+++ ExpertSpring30/src/main/java/aop005/Start.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //aop004 : POJO XML기반으로 aspect를 구현하는 예시 / before, after를 구현함.
-public class Start { //commit test pc nb pc2 main
+public class Start { //commit test pc nb pc2 main / pc-nb test
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("aop005/aop005.xml");
